75adf8eed0363c420f9e79bbdbe01ff9187f9d21
suhussai <suhussai@ualberta.ca>
2018-12-30 21:05:49 -0700

    add blockchain blueprint class

---
+++ blockchain.py
@@ -0,0 +1,10 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        pass
+
+    def new_transaction(self):
+        pass
